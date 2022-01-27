181cdeba0364208b5a5d85fd172987fe67c6fb30
XbozzX <muhammad_19000649@utp.edu.my>
2022-01-27 09:53:11 +0800

    Release the full code to the main branch

--- src/com/learnProgramming/ComplexNumber.java
+++ src/com/learnProgramming/ComplexNumber.java
@@ -1,4 +1,47 @@
 package com.learnProgramming;
 
 public class ComplexNumber {
+    //init the fields
+    private double real;
+    private double imaginary;
+
+    //declare the constructor for init the fields
+    public ComplexNumber(double real, double imaginary){
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    //declare the getter for real and imaginary
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+
+    // Create a method for adding the real and imaginary number
+    public void add(double real, double imaginary){
+        this.real += real;
+        this.imaginary += imaginary;
+    }
+
+    // Create a method for adding the existing real and imaginary number
+    public void add(ComplexNumber Number){
+        this.real += Number.real;
+        this.imaginary += Number.imaginary;
+    }
+
+    // Create a method for subtract the real and imaginary number
+    public void subtract(double real, double imaginary){
+        this.real -= real;
+        this.imaginary -= imaginary;
+    }
+
+    // Create a method for subtract the existing real and imaginary number
+    public void subtract(ComplexNumber Number){
+        this.real -= Number.real;
+        this.imaginary -= Number.imaginary;
+    }
 }
--- src/com/learnProgramming/Main.java
+++ src/com/learnProgramming/Main.java
@@ -4,5 +4,33 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        //User will insert the input inside the class
+        ComplexNumber one = new ComplexNumber(1.0,1.0);
+        ComplexNumber Number = new ComplexNumber(2.5,-1.5);
+
+        //User call add method with the adding real number and imaginary number
+        //as a parameter
+        one.add(1,1);
+
+        //Display the result into console
+        System.out.println("one.real= " + one.getReal());
+        System.out.println("one.imaginary= " + one.getImaginary());
+
+        //User call subtract method with adding Number as an object
+        // to be the parameter
+        one.subtract(Number);
+
+        //Display the result into console
+        System.out.println("=========================================");
+        System.out.println("one.real= " + one.getReal());
+        System.out.println("one.imaginary= " + one.getImaginary());
+        System.out.println("=========================================");
+
+        //User call subtract method with adding One as an object
+        // to be the parameter
+        Number.subtract(one);
+        System.out.println("Number.real= " + Number.getReal());
+        System.out.println("Number.imaginary= " + Number.getImaginary());
+
     }
 }
